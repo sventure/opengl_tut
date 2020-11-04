66cfcaeb1bbc8073d454db83e371bcf12a7f83c4
sventure <sidswamy2002@gmail.com>
2020-11-04 22:17:28 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,24 +2,31 @@
 
 #include"libs.h"
 
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"                                                                          //a basic vertex shader
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"                                                                          //orange fragment shader
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
 using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
+
 int main()
 {
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,                     //triangle vertices
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f,
-	};	
-
 	/* In the main function we first initialize GLFW with glfwInit, after which we can configure GLFW using glfwWindowHint.
 	The first argument of glfwWindowHint tells us what option we want to configure, where we can select the option from a
 	large enum of possible options prefixed with GLFW_. The second argument is an integer that sets the value of our option. */
-
-
 
 	glfwInit();                                                         //initialize glfw
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                      //sets major and minor version to 3.0+
@@ -36,16 +43,48 @@ int main()
 	glfwMakeContextCurrent(window);
 
 
-
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		cout << "Failed to initialize GLAD" << endl;                            //initialize glad
 		return -1;
 	}
 
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);                            //create vertex shader
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);                                              //attach created vertex shader to the vertex shader source (init at the top of the code)
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	                                                                            
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;            //checks if vertex shader works
+	}
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+
+
+
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,                     //triangle vertices
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f,
+	};
+
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);                                                      //generate a vertex buffer
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);                                         //binds the created buffer to GL_ARRAY_BUFFER buffer type
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);  //copies the buffer info to the currently bound buffer
+
+	/* ^^^  1st argument is the type of the buffer we want to copy data into
+	        2nd argument specifies the size of the data (in bytes) we want to pass to the buffer
+			3rd parameter is the actual data we want to send
+			4th parameter specifies how we want the graphics card to manage the given data. */
 
 	glViewport(0, 0, 800, 600);                                                 //create viewport
 	
